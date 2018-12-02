972395df2af96aa5a18512adf27870187b05a60a
Configural <kashkanov_as@mail.ru>
2018-12-02 16:48:17 +0300

    a.kashkanov - to contscts

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -7,17 +7,35 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
+        
+        <div class="col-md-2">
+            
+        <div class="panel panel-default">
+            <div class="panel-heading">Об авторе проекта
+                </div>
 
+                <div class="panel-body">
+                    <p><img src="http://www.artem-kashkanov.ru/photos/3/avatar.jpg" class="avatar"></p>
+                    <p>Меня зовут Артем Кашканов. Я хотел стать фотографом, но что-то пошло не так и я стал программистом. Тем не менее, мое основное хобби - фотография :)</p>
+                    <p>Именно это сочетание навыков породило <a href="http://www.artem-kashkanov.ru" target="_blank">Фотосайт Артема Кашканова</a> и данный Фотоклуб. </p>
+                    <p>На правах разработчика, администратора, владельца проекта, а также бывшего преподавателя фотографии хочу предложить вашему вниманию мои <a href="http://artem-kashkanov.ru/pdf-books.html" target="_blank">электронные книги</a>.</p>
+
+            </div>
+        </div>
+            
+        </div>
+        
         <div class="col-md-8 col-md-offset-2">
             
             <h1>Контакты</h1>
-            <p>Администратор фотоклуба: Артем Кашканов</p>
+            <p class="lead">
             <ol>
                 <li>Email: kashkanov_as@mail.ru</li>
                 
                 <li>Телефон: +79202525268</li>
                 
             </ol>
+        </p>
 
         </div>
     </div>
