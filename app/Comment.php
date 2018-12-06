2d666f8287b986f98ed66ef1bb349f99e1907884
Configural <configural@yandex.ru>
2018-12-06 10:54:05 +0300

    bb-codes updated

--- app/Comment.php
+++ app/Comment.php
@@ -19,16 +19,22 @@ class Comment extends Model
         
 
     public function bbCode($text){
-        $text = str_ireplace("[b]", "<strong>", $text);
-        $text = str_ireplace("[/b]", "</strong>", $text);
 
-        $text = str_ireplace("[i]", "<i>", $text);
-        $text = str_ireplace("[/i]", "</i>", $text);
-
-        $text = str_ireplace("[img]", "<br/><img src='", $text);
-        $text = str_ireplace("[/img]", "'>", $text);
+    $text = preg_replace('/\[(\/?)(b|i|u|s)\s*\]/', "<$1$2>", $text);
+    
+    $text = preg_replace('/\[code\]/', '<pre><code>', $text);
+    $text = preg_replace('/\[\/code\]/', '</code></pre>', $text);
+    
+    $text = preg_replace('/\[(\/?)quote\]/', "<$1blockquote>", $text);
+    $text = preg_replace('/\[(\/?)quote(\s*=\s*([\'"]?)([^\'"]+)\3\s*)?\]/', "<$1blockquote>Цитата $4:", $text);
+    
+    $text = preg_replace('/\[url\](?:http:\/\/)?([a-z0-9-.]+\.\w{2,4})\[\/url\]/', "<a href=\"http://$1\">$1</a>", $text);
+    $text = preg_replace('/\[url\s?=\s?([\'"]?)(?:http:\/\/)?([a-z0-9-.]+\.\w{2,4})\1\](.*?)\[\/url\]/', "<a href=\"http://$2\">$3</a>", $text);
+    
+    
+    $text = preg_replace('/\[img\s*\]([^\]\[]+)\[\/img\]/', "<p><img src='$1'/></p>", $text);
+    $text = preg_replace('/\[img\s*=\s*([\'"]?)([^\'"\]]+)\1\]/', "<p><img src='$2'/></p>", $text);
         
-        $text = str_ireplace("\r\n", "<br/>", $text);
 
     return $text;
     
