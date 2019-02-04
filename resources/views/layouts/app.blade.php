b903bb42d7ef22b168a3ebc30d9eee5a5d1f6679
Configural <configural@yandex.ru>
2019-02-04 10:24:30 +0300

    ya counter updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -151,41 +151,25 @@ theme_advanced_resizing : true,
             <div class="col-sm-2">Возрастное ограничение 16+</div>
             <div class="col-sm-2">
                 
-                <!-- Yandex.Metrika informer -->
-<a href="https://metrika.yandex.ru/stat/?id=3048703&amp;from=informer"
-target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/3048703/2_1_FFFFFFFF_FFFFFFFF_0_pageviews"
-style="width:80px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры)" class="ym-advanced-informer" data-cid="3048703" data-lang="ru" /></a>
-<!-- /Yandex.Metrika informer -->
-
+                
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript" >
-    (function (d, w, c) {
-        (w[c] = w[c] || []).push(function() {
-            try {
-                w.yaCounter3048703 = new Ya.Metrika2({
-                    id:3048703,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-                });
-            } catch(e) { }
-        });
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/tag.js";
-
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else { f(); }
-    })(document, window, "yandex_metrika_callbacks2");
+   ym(52212658, "init", {
+        id:52212658,
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/3048703" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->&nbsp;
+<noscript><div><img src="https://mc.yandex.ru/watch/52212658" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+                
+                &nbsp;
 <!--LiveInternet counter--><script type="text/javascript">
 document.write("<a href='//www.liveinternet.ru/click' "+
 "target=_blank><img src='//counter.yadro.ru/hit?t12.2;r"+
