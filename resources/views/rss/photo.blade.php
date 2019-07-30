6f86fe169f58f6f3875ede7251059bb3fb80e5d8
Configural <configural@yandex.ru>
2019-07-30 14:11:55 +0300

    rss feed 4

--- resources/views/rss/photo.blade.php
+++ resources/views/rss/photo.blade.php
@@ -9,12 +9,10 @@
     <lastBuildDate>Tue, 30 Jul 2019 10:39:17 +0300</lastBuildDate>
     @foreach($photo as $p)
     <item>
-      <title><![CDATA[{{ $p->name}}]]></title>
+      <title><![CDATA[{{ $p->user->name}}:  {{ $p->name}}]]></title>
       <link>http://club.artem-kashkanov.ru/photo/{{$p->id}}</link>
-      <description><![CDATA[{{$p->description}}]]></description>
-      <content:encoded><![CDATA[<img src=http://club.artem-kashkanov.ru/photos/{{$p->user_id}}/{{$p->url}}>]]</content:encoded>
-      <dc:creator xmlns:dc="http://purl.org/dc/elements/1.1/">{{$p->user->name}}</dc:creator>
-      <pubDate>{{$p->created_at->format(DateTime::RSS)}}</pubDate>
+      <description><![CDATA[<img src=http://club.artem-kashkanov.ru/photos/{{$p->user_id}}/{{$p->url}}>]]</description>
+     <pubDate>{{$p->created_at->format(DateTime::RSS)}}</pubDate>
     </item>
     @endforeach
   </channel>
