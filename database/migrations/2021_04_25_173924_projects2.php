a2531c747b1bb9fc7be68fc6d55047485589d9cc
Artem Kashkanov <kashkanov_as@mail.ru>
2021-04-25 17:40:22 +0300

    projects_migration

--- database/migrations/2021_04_25_173924_projects2.php
+++ database/migrations/2021_04_25_173924_projects2.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Projects extends Migration
+class Projects2 extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,15 @@ class Projects extends Migration
      */
     public function up()
     {
+        //
+                //
 
+        
+        Schema::create('photos2project', function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('photo_id')->nullable();
+            $table->integer('project_id')->nullable();
+        });
     }
 
     /**
