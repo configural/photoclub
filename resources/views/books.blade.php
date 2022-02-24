c3eec440348d75bd92e6f5354ee45bb049ba826d
Artem Kashkanov <kashkanov_as@mail.ru>
2022-02-24 21:21:49 +0300

    booksale stopped

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -12,24 +12,12 @@
             
             <h1>Электронные книги о фотографии</h1>
             
-            <p class="lead">В этом разделе вы можете за разумную цену приобрести электронные книги о фотографии. Вы получите интересные и полезные материалы и заодно поддержите проект &laquo;Фотоклуб Артема Кашканова&raquo;!</p>
-            <p>
-                Книги ориентированы в первую очередь на начинающих фотолюбителей, желающих разобраться в тонкостях фотодела. Они написаны простым доступным языком и вы найдете в них ответы практически на все вопросы, касающиеся фотографии.
-            </p>
-            <p>
-                После оплаты вы получите на электронную почту ссылку для скачивания материала. Вы ничем не рискуете - если книга вам не понравится (что маловероятно), у вас есть возможность вернуть потраченные деньги в течение 24 часов с момента оплаты.
-            </p>
-            <p>Если у вас есть какие-то вопросы, касающиеся приобретения электронных книг, или возникли проблемы с доставкой электронных писем обращайтесь, мы решим все вопросы!
-            </p>
-            <p><a href="{{url('/contacts')}}" class="btn btn-primary">Перейти в раздел &laquo;Контакты&raquo;</a>
+            <p class="lead">Автоматическая продажа книг по техническим причинам приостановлена. 
+                Если хотите получить полный комплект электронных материалов «О фотографии простым языком»,
+                пишите мне на почту <a href="mailto:configural@yandex.ru">configural@yandex.ru</a>.
             </p>
             
-    <script>!function(e){var l=function(l){return e.cookie.match(new RegExp("(?:^|; )digiseller-"+l+"=([^;]*)"))},i=l("lang"),s=l("cart_uid"),t=i?"&lang="+i[1]:"",d=s?"&cart_uid="+s[1]:"",r=e.getElementsByTagName("head")[0]||e.documentElement,n=e.createElement("link"),a=e.createElement("script");n.type="text/css",n.rel="stylesheet",n.id="digiseller-css",n.href="//shop.digiseller.ru/xml/store2_css.asp?seller_id=823383",a.async=!0,a.id="digiseller-js",a.src="//www.digiseller.ru/store2/digiseller-api.js.asp?seller_id=823383"+t+d,!e.getElementById(n.id)&&r.appendChild(n),!e.getElementById(a.id)&&r.appendChild(a)}(document);</script>
-
-    <span class="digiseller-body" id="digiseller-body" data-partner-id="" data-cat="0" data-logo="0" data-downmenu="0" data-purchases="1" data-langs="0" data-cart="1" data-search="0"></span>
-
-
-
+    
 
         </div>
     </div>
