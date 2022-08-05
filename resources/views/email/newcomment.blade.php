0b7a3aa844ba5d43e7bc3643a6391a3d37aa4d06
Artem Kashkanov <kashkanov_as@mail.ru>
2022-08-05 18:06:31 +0300

    email comment

--- resources/views/email/newcomment.blade.php
+++ resources/views/email/newcomment.blade.php
@@ -1,14 +1,14 @@
 <p>Здравствуйте, {{ $user }}</p>
 
-<p>К Вашей фотоработе написали комментарий.</p>
+<p>К Вашей фотографии написали комментарий.</p>
 <p><i><strong>{{ $comment_user }}:</strong>
         {!! $comment_text!!}</i>
 </p>
 <p>
-    <a href="http://club.artem-kashkanov.ru/photo/{{ $photo_id}}">Перейти к фотоработе</a>
+    <a href="https://diafragma8.ru/photo/{{ $photo_id }}">Перейти к фотоработе</a>
 </p>
 <hr>	
 <p>Письмо сформировано роботом, отвечать на него не надо.</p>
-<p>Фотоклуб Артема Кашканова</p>
-<p><a href="http://club.artem-kashkanov.ru">http://club.artem-kashkanov.ru</a>
+<p>Фотоклуб "Диафрагма 8"</p>
+<p><a href="https://diafragma8.ru">https://diafragma8.ru</a>
 
